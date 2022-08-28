3e7844772e5b5dfb54f9ba0dd144200762ab1f2d
Thomas Moleski <thomasmoleski@Thomass-MacBook-Pro.local>
2022-08-28 12:03:53 -0400

    Commit 1 - added variables

---
+++ Develop/script.js
@@ -0,0 +1,28 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// all characters and options for password 
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!","@","#","$","%","^","&","*","(",")","'","+","-",".",":",";",",","/","<",">","=","?","{","}","|","~"];
+
+
+
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+
+  passwordText.value = password;
+
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
