1fe7cfb57abfcc7ac2ea27a977c184ecebbe34d9
Thomas Moleski <thomasmoleski@Thomass-MacBook-Pro.local>
2022-08-28 14:41:25 -0400

    Commit 2 - If/Else statements

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,67 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// all characters and options for password 
 
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["!","@","#","$","%","^","&","*","(",")","'","+","-",".",":",";",",","/","<",">","=","?","{","}","|","~"];
+function generatePassword() {
+
+  // all characters and options for password 
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "'", "+", "-", ".", ":", ";", ",", "/", "<", ">", "=", "?", "{", "}", "|", "~"];
+
+  //questions to ask user 
+  numberOfDigits = prompt("How many characters would you like your password to contain?");
+  if (numberOfDigits < 8 || numberOfDigits > 128) {
+    prompt("Your password must be more than 8 characters or less than 128 characters");
+  }
+  else {
+    alert("Your password will be " + numberOfDigits + " characters");
+  }
+
+
+  returnUpperCase = confirm("Do you want to use upper case letters?");
+  if (returnUpperCase = true) {
+    alert("Password will contain Upper Case letters!");
+  }
+  else {
+    alert("Password will not contain Upper Case");
+  }
+  returnLowerCase = confirm("Do you want to use lower case letters?");
+  if (returnLowerCase = true) {
+    alert("Password will contain Lower Case Letters!");
+  }
+  else { 
+    alert("Password will not contain Lower Case Letters!");
+  } 
+
+  returnNumbers = confirm("do you want to use Numbers?");
+  if (returnNumbers = true) {
+    alert("Password will contain Numbers!");
+  }
+  else {
+    alert("Password will not contain Numbers!");
+  }
+
+  returnSpecial = confirm("Do you want to use Special Characters?");
+  if (returnSpecial = true) {
+    alert("Password will contain Special Characters!");
+  }
+  else {
+    alert("Password will not contain Special Characters!");
+  }
 
 
 
+
+
+
+
+
+
+
+
+}
 //Write password to the #password input
 function writePassword() {
   var password = generatePassword();
